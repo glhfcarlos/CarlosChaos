6ba1d766a8d2983dfa9a5b41412ed51c3fcb23cb
glhfcarlos <glhfcarlos@gmail.com>
2024-12-08 15:20:52 -0800

    all this

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/CarlosChaos/', // Replace 'repository-name' with your actual GitHub repository name
+  build: {
+    outDir: 'dist', // This is the default output folder for Vite builds
+  },
+});
